dc5905b862632293405aaf574b87e383d8f0ac8b
Massidama <massidama@outlook.it>
2024-02-09 19:02:45 +0100

    feat: modifiche main

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,115 @@
 <footer>
-    <h1>Hello from footer</h1>
+<section class="blue-bar">
+        <div class="container">
+
+            <!-- prima parte del footer (info) -->
+            <div class="infobar">
+                <div class="info-item">
+                    <img src="../../public/img/buy-comics-digital-comics.png" alt="">
+                    <h6> DIGITAL COMICS</h6>
+                </div>
+                <div class="info-item">
+                    <img src="../../public/img/buy-comics-merchandise.png" alt="">
+                    <h6> DC MERCHANDISE</h6>
+                </div>
+                <div class="info-item">
+                    <img src="../../public/img/buy-comics-subscriptions.png" alt="">
+                    <h6> SUBSCRIPTIONS</h6>
+                </div>
+                <div class="info-item">
+                    <img src="../../public/img/buy-comics-shop-locator.png" alt="">
+                    <h6> COMIC SHOP LOCATOR</h6>
+                </div>
+                <div class="info-item">
+                    <img src="../../public/img/buy-comics-digital-comics.png" alt="ciao">
+                    <h6> DC POWER VISA</h6>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="footer-info">
+
+       <div class="container">
+              <!-- parte info section -->
+            <div class="info-content">
+                <div class="info-section">
+                    <h4> DC COMICS </h4>
+                    <ul>
+                        <li><a href="#"> Characters </a></li>
+                        <li><a href="#"> Comics </a></li>
+                        <li><a href="#"> Movies </a></li>
+                        <li><a href="#"> TV </a></li>
+                        <li><a href="#"> Games </a></li>
+                        <li><a href="#"> Videos </a></li>
+                        <li><a href="#"> News </a></li>
+                    </ul>
+
+                    <h4> SHOP </h4>
+                    <ul>
+                        <li><a href="#"> Shop DC </a></li>
+                        <li><a href="#"> Shop DC Collectibles </a></li>
+                    </ul>
+                </div>
+
+                <!-- seconda colonna info section -->
+                <div class="info-section">
+                    <h4> DC </h4>
+                    <ul>
+                        <li><a href="#"> Terms Of Use </a></li>
+                        <li><a href="#"> Privacy policy (New) </a></li>
+                        <li><a href="#"> Ad Choices </a></li>
+                        <li><a href="#"> Advertising </a></li>
+                        <li><a href="#"> Jobs </a></li>
+                        <li><a href="#"> Subscriptions </a></li>
+                        <li><a href="#"> Talent Workshops </a></li>
+                        <li><a href="#"> CPSC Certificates </a></li>
+                        <li><a href="#"> Ratings </a></li>
+                        <li><a href="#"> Shop Help </a></li>
+                        <li><a href="#"> Contact Us </a></li>
+                    </ul>
+                </div>
+
+                <!-- terza colonna info section -->
+                <div class="info-section">
+                    <h4> SITES </h4>
+                    <ul>
+                        <li><a href="#"> DC </a></li>
+                        <li><a href="#"> MAD Magazine </a></li>
+                        <li><a href="#"> DC Kids </a></li>
+                        <li><a href="#"> DC Universe </a></li>
+                        <li><a href="#"> DC Power Visa </a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="logo-footer">
+                
+            </div>
+       </div>
+
+      
+
+    </section>
+
+    <!-- barra finale contatti social -->
+    <section class="gray-bar">
+        <div class="container">
+
+            <div class="sign-up">
+                SIGN-UP NOW!
+            </div>
+
+            <div class="social-contact">
+                <span> FOLLOW US</span>
+                <a href="#"><img src="/img/footer-facebook.png" alt="Facebook"></a>
+                <a href="#"><img src="/img/footer-twitter.png" alt="Twitter"></a>
+                <a href="#"><img src="/img/footer-youtube.png" alt="YouTube"></a>
+                <a href="#"><img src="/img/footer-pinterest.png" alt="Pinterest"></a>
+                <a href="#"><img src="/img/footer-periscope.png" alt="Periscope"></a>
+            </div>
+        </div>
+    </section>
+
+
 </footer>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,8 +1,34 @@
 @extends('layouts.main-layout')
 
-@section('head')
-    <title>Home</title>
-@endsection
 @section('content')
-    <h1>Hello, World!</h1>
+
+
+
+        
+
+            <section class="black-bar">
+
+                <div class="container-cards">
+                @foreach ($comics as $comic)
+                    <div class="cards">
+                        <div class="prod-card">
+                            <img src="https://www.rivistaundici.com/wp-content/uploads/2023/05/GettyImages-1467624093.jpg?x59145" alt="">
+                        </div>
+                        <div class="thumb-title">
+                            {{$comic["series"]}}
+                        </div>
+                    </div>
+                @endforeach
+                    <div class="button">
+                        <a href="#"> LOAD MORE </a>
+                    </div>
+                </div>
+
+            </section>
+            
+       
+
 @endsection
+
+
+
